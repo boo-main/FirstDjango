4807a6e2fd6de63a3d6b070bcf561fb29cf2a921
boo <eyurchenko@specialist.ru>
2023-02-15 23:56:46 +0700

    Django-template start

--- MainApp/views.py
+++ MainApp/views.py
@@ -15,15 +15,16 @@ items = [
     {"id": 5, "name": "Coca-cola 1 литр", "quantity": 12},
     {"id": 7, "name": "Картофель фри", "quantity": 0},
     {"id": 8, "name": "Кепка", "quantity": 124},
+    {"id": 10, "name": "Стул", "quantity": 2},
 ]
 
 
 def home(request):
-    result = """
-    <h1>"Изучаем django"</h1>
-    <strong>Автор</strong>: <i>Иванов И.П.</i>
-    """
-    return HttpResponse(result)
+    context = {
+        "name": "Евгений",
+        "email": "email@test.ru"
+    }
+    return render(request, 'index.html', context)
 
 
 def about(request):
@@ -59,8 +60,7 @@ def get_item(request, id):
 #     <li>....</li>
 # </ol>
 def items_list(request):
-    result = "<h2>Список товаров</h2><ol>"
-    for item in items:
-        result += f"<li><a href='/item/{item['id']}'>{item['name']}</a></li>"
-    result += "</ol>"
-    return HttpResponse(result)
+    context = {
+        "items": items
+    }
+    return render(request, 'items-list.html', context)
