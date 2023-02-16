23135c2f2bc44fc2ffc7d6f63818c9ba97a64d23
boo <eyurchenko@specialist.ru>
2023-02-17 01:10:35 +0700

    Module-3 tasks part-2 complete

--- MainApp/models.py
+++ MainApp/models.py
@@ -5,3 +5,4 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     count = models.PositiveIntegerField()
+    description = models.TextField(max_length=1000, default="Базовое описание")
