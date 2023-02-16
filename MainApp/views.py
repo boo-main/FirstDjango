ea7a4de16180224e67bc4c4ace356b7253f4cd9c
boo <eyurchenko@specialist.ru>
2023-02-17 00:25:06 +0700

    Module-3 tasks part-1 complete

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from django.http import HttpResponseNotFound
 from MainApp.models import Item
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def home(request):
@@ -25,14 +26,16 @@ def about(request):
 # url: /item/1
 # url: /item/2
 def get_item(request, id):
-    for item in items:
-        if item['id'] == id:
-            context = {
-                'item': item
-            }
-            return render(request, 'item-page.html', context)
+    try:
+        item = Item.objects.get(id=id)
+    except ObjectDoesNotExist:
+        return HttpResponseNotFound(f"Item with id={id} not found")
+    context = {
+        'item': item
+    }
+    return render(request, 'item-page.html', context)
 
-    return HttpResponseNotFound(f"Item with id={id} not found")
+    #
 
 
 def items_list(request):
