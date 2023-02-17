e4f9cb9db244c47f3bcad5f46ca96324eb841927
boo <eyurchenko@specialist.ru>
2023-02-18 00:02:23 +0700

    static files

--- FirstDjango_14_02/urls.py
+++ FirstDjango_14_02/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from MainApp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('about', views.about, name='about'),
     path('item/<int:id>', views.get_item, name='item-detail'),
     path('items', views.items_list, name="items-list"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # Сеошники
 # item --> item-page --> get-item
